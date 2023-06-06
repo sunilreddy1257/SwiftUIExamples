a1e752b8d3498fc9c6b45e8ebb1f9268e2bf2084
sunilreddy1257 <sunilreddy1257@gmail.com>
2023-06-06 20:59:13 +0530

    SwiftUI app added

---
+++ SwiftUIExamples/SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Sunil Kumar Reddy Sanepalli on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
